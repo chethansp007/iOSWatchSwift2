6e30fda5f91e4cdc18421a3341cf02698592f546
chethansp007 <chethan.palaksha@tigerspike.com>
2016-08-28 19:57:25 +0800

    Update test.sh
    From Git.

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 export DEVELOPER_DIR=/Applications/Xcode.app/Contents/Developer/
-xcodebuild -workspace iOSWatchSwift2.workspace -scheme iOSWatchSwift2 -archivePath builds/iOSWatchSwift2.xcarchive archive
+xcodebuild -workspace iOSWatchSwift2.xcworkspace -scheme iOSWatchSwift2 -archivePath builds/iOSWatchSwift2.xcarchive archive
 
 xcodebuild -exportArchive -exportOptionsPlist exportPlist.plist -archivePath builds/iOSWatchSwift2.xcarchive -exportPath builds
